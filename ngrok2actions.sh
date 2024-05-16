7f03bfd5ba7e51e9b56a2926a9e03039ffe33ef9
Hosh <hoshsadiq@users.noreply.github.com>
2024-05-16 17:02:47 +0100

    Update ngrok2actions.sh

--- ngrok2actions.sh
+++ ngrok2actions.sh
@@ -19,7 +19,7 @@ install_tmate() {
     latest_tmate_version="$(curl --fail --show-error --silent --location "https://api.github.com/repos/tmate-io/tmate/releases/latest" | jq -r .tag_name)"
     if uname --kernel-name --machine | grep -qFxi "Linux x86_64"; then
         echo -e "${INFO} Install tmate ..."
-    
+
         curl --fail --show-error --silent --location "https://github.com/tmate-io/tmate/releases/download/${latest_tmate_version}/tmate-${latest_tmate_version}-static-linux-amd64.tar.xz" | \
           sudo tar --xz -xvf - -C /usr/local/bin --strip-components=1
     else
@@ -31,56 +31,59 @@ install_tmate() {
 }
 
 set_up_authorized_keys() {
-    echo -e "${INFO} Setting up authorized_keys for user ${GITHUB_ACTOR}:"
+    echo -e "${INFO} Setting up authorized_keys."
     mkdir -p "$HOME/.ssh"
     curl -fsSL "https://github.com/${GITHUB_ACTOR}.keys" > "$HOME/.ssh/authorized_keys"
     chmod -R go-rwx "$HOME/.ssh"
-    
-    echo -e "${INFO} The following keys will have access:"
-    echo
-    ssh-keygen -l -f /dev/stdin < "$HOME/.ssh/authorized_keys" | sed -e "s/^/${green}    /" -e "s/\$/${reset}/"
-    echo
 }
 
-install_tmate
-set_up_authorized_keys
+start_tmate() {
+  echo -e "${INFO} Start tmate..."
+  exec tmate -a "$HOME/.ssh/authorized_keys" -S "$SOCKET_FILE" -F >"$LOG_FILE" 2>&1 &
+  TMATE_PID="$!"
 
-echo -e "${INFO} Start tmate..."
-exec tmate -a "$HOME/.ssh/authorized_keys" -S "$SOCKET_FILE" -F >"$LOG_FILE" 2>&1 &
-TMATE_PID="$!"
+  sleep 1
+}
 
-sleep 1
-
-ERRORS_LOG=$(grep "command failed" "${LOG_FILE}" || true)
-if [[ -e "${LOG_FILE}" && -z "${ERRORS_LOG}" ]]; then
-    SSH_CMD="$(awk '/^ssh session:/{print $4}' "$LOG_FILE")"
-    PRT_COUNT=0
-    while pgrep -x tmate >/dev/null && [[ ! -f "$CONTINUE_FILE" ]]; do
-      if ((PRT_COUNT % 10 == 0)); then
-        echo -e "$(cat <<EOF
+print_connection_info() {
+  local key_fingerprints
+  key_fingerprints="$(ssh-keygen -l -f /dev/stdin < "$HOME/.ssh/authorized_keys" | sed -e "s/^/${green}    /" -e "s/\$/${reset}/")"
+  echo -e "$(cat <<EOF
 ------------------------------------------------------------------------
 To connect to this session copy and paste the following into a terminal:
 
     ${green}ssh ${SSH_CMD}${reset}
 
 ${yellow}NOTE:${reset} you must authenticate using the private key of any SSH key set up
-in your GitHub account. You can use the ${green}-i /path/to/private-key${reset} with ssh.
+in your GitHub ${GITHUB_ACTOR} account. You can use the ${green}-i /path/to/private-key${reset} with ssh.
+
+${key_fingerprints}
 
 ${yellow}TIP:${reset} Run ${green}touch ${CONTINUE_FILE}${reset} to continue to the next step.
 ------------------------------------------------------------------------
 EOF
 )"
+}
 
-        echo -e "${INFO} Re-printing help in 10 seconds ..."
-      fi
-
+await_continue() {
+  ERRORS_LOG=$(grep "command failed" "${LOG_FILE}" || true)
+  if [[ -e "${LOG_FILE}" && -z "${ERRORS_LOG}" ]]; then
+    SSH_CMD="$(awk '/^ssh session:/{print $4}' "$LOG_FILE")"
+    while pgrep -x tmate >/dev/null && [[ ! -f "$CONTINUE_FILE" ]]; do
       sleep 1
-      ((PRT_COUNT = PRT_COUNT+1))
     done
 
     echo -e "${WARN} Continue to the next step."
     kill "$TMATE_PID"
-else
+  else
     echo "${ERRORS_LOG}"
     exit 4
-fi
+  fi
+}
+
+
+install_tmate
+set_up_authorized_keys
+start_tmate
+print_connection_info
+await_continue
